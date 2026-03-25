53cdc061e06b44bcd95d2cff4ee00e58ed14fd49
mairup <maimairupnik@gmail.com>
2026-03-25 15:58:53 +0100

    Initialization

---
+++ src/main/java/uv/naloge/druga/druganaloga/Launcher.java
@@ -0,0 +1,9 @@
+package uv.naloge.druga.druganaloga;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
